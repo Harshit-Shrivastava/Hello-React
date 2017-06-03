2fe81e57f0655f5ebb8a66f8e0e161386bfcd64c
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2017-06-03 19:44:52 -0400

    Refactoring

--- public/app.jsx
+++ public/app.jsx
@@ -1,82 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var GreeterMessage = React.createClass({
-  render: function () {
-    var name= this.props.name;
-    var message = this.props.message;
-    return (
-      <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}</p>
-      </div>
-    );
-  }
-});
-
-var GreeterForm = React.createClass({
-  onFormSubmit: function (e) {
-    e.preventDefault();
-    var updates = {};
-    var name = this.refs.name.value;
-    var message = this.refs.message.value;
-    if(name.length>0){
-      this.refs.name.value = '';
-      updates.name = name;
-    }
-    if(message.length>0){
-      this.refs.message.value = '';
-      updates.message = message;
-    }
-    this.props.onNewData(updates);
-  },
-  render: function () {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <div>
-            <input type="text" placeholder="Enter name" ref="name" /><br/>
-        </div>
-        <div>
-            <textarea placeholder="Enter message" ref="message" /><br/>
-        </div>
-        <div>
-            <button>Submit</button>
-        </div>
-      </form>
-    );
-  }
-});
-
-var Greeter = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'React',
-      message: "This is from the default prop."
-    };
-  },
-  getInitialState: function () {
-    return{
-      name: this.props.name,
-      message: this.props.message
-    };
-  },
-  handleNewData: function (updates) {
-    this.setState(updates);
-  },
-  render: function () {
-    var name = this.state.name;
-    var message = this.state.message;
-    return (
-      <div>
-        <GreeterMessage name={name} message={message} />
-        <GreeterForm onNewData={this.handleNewData}/>
-      </div>
-    );
-  }
-});
+var Greeter = require('./components/Greeter');
 
 var firstName = "Harshit";
-var customMessage = "This is from a component."
+var customMessage = "This is the default message."
 
 ReactDOM.render(
   <Greeter name={firstName} message={customMessage}/>,
---
+++ public/components/GreeterForm.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function (e) {
+    e.preventDefault();
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+    if(name.length>0){
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+    if(message.length>0){
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+    this.props.onNewData(updates);
+  },
+  render: function () {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+            <input type="text" placeholder="Enter name" ref="name" /><br/>
+        </div>
+        <div>
+            <textarea placeholder="Enter message" ref="message" /><br/>
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
+});
+
+module.exports = GreeterForm;
